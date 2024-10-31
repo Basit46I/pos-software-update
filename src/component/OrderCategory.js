0bf640eff2d860b4c60fcab1d717dee0e75946b8
Basit46I <basitprogrammer46@gmail.com>
2024-10-31 12:00:25 +0500

    31may2024_2

--- src/component/OrderCategory.js
+++ src/component/OrderCategory.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
 const OrderCategory = () => {
-
     const categories = [
-        { id: 1, name: "Burger" },
-        { id: 2, name: "Pizza" },
-        { id: 3, name: "Dessert" },
-        { id: 4, name: "Beverages" },
-        { id: 5, name: "Pizza" },
-        { id: 6, name: "Dessert" },
+        { id: 1, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 2, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 3, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 4, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 5, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 6, name: "Burger", img: "images/category-images/burger-image.jpg" },
+        { id: 7, name: "Burger", img: "images/category-images/burger-image.jpg" },
     ]
 
     return (
-        <div className="d-flex align-items-center justify-content-between" style={{ width: "66.5%" }}>
+        <div className="d-flex flex-wrap justify-content-center align-items-center" style={{ width: "50%" }}>
             {categories.map(category => (
-                <div key={category.id} className="p-3 bg-white shadow-sm text-center rounded" style={{ width: "10%" }}>
-                    {category.name}
+                <div key={category.id} className="p-1 m-1 bg-white shadow-sm rounded d-flex flex-column align-items-center" style={{ width: "100px", height: "100px" }}>
+                    <div style={{ width: "45px", height: "45px" }}>
+                        <img className="w-100 h-100" src={category.img} alt={category.name} />
+                    </div>
+                    <div className="text-center" style={{ fontSize: "0.80rem" }}>
+                        {category.name}
+                    </div>
                 </div>
             ))}
         </div>
